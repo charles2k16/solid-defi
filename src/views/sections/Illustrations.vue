4b32dbd05a86adebb5d675134ee15d4b22036dc2
Charles Obeng <charlesobeng000@gmail.com>
2021-06-11 16:09:10 +0000

    presale

--- src/views/sections/Illustrations.vue
+++ src/views/sections/Illustrations.vue
@@ -1,30 +1,34 @@
 <template>
   <div>
     <!-- timer for tokens -->
-    <!-- <vs-row class="mt-50">
+    <vs-row class="mt-50">
       <vs-col :w="6" :xs="12" :sm="12">
-        <div class="flex-align-center">
-          <h1 class="time_left">Time Left</h1>
-          <flip-countdown
-            deadline="2021-06-11 00:00:00"
-            class="timerr"
-          ></flip-countdown>
+        <div>
+          <h1 class="time_left">COUNTDOWN TO</h1>
+          <h2 class="time_left-2">COMMUNITY PRESALE</h2>
         </div>
       </vs-col>
       <vs-col :w="1" :xs="2" :sm="2">
-        <div style="text-align:center;margin-left:-40px;padding-top:15px">
+        <div style="text-align:center;margin-left:-40px;padding-top:40px">
           <div class="green_rule"></div>
         </div>
       </vs-col>
       <vs-col :w="5" :xs="12" :sm="12">
-        <div style="maring-top:50px;padding-top:15px">
-          <div class="progress">
+        <div style="margin-top:-15px">
+          <!-- <div class="progress">
             <div class="progress_time"></div>
           </div>
-          <p class="mt-20 p_tokens">5 days</p>
+          <p class="mt-20 p_tokens">5 days</p> -->
+
+          <flip-countdown
+            deadline="2021-06-15 00:00:00"
+            class="timerr"
+          ></flip-countdown>
+
+          <h3 class="june-dd">JUNE 21 2021 5pm UTC</h3>
         </div>
       </vs-col>
-    </vs-row> -->
+    </vs-row>
 
     <vs-row class="mt-70">
       <vs-col :w="12" :xs="12" :sm="12">
@@ -55,12 +59,12 @@
 </template>
 
 <script>
-// import FlipCountdown from 'vue2-flip-countdown';
+import FlipCountdown from 'vue2-flip-countdown';
 
 export default {
   name: 'Illustrations',
-  // components: {
-  //   FlipCountdown,
-  // },
+  components: {
+    FlipCountdown,
+  },
 };
 </script>
